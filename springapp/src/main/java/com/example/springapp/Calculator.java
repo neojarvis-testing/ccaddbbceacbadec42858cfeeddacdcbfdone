a76d38208eb1858115ba8842b0cc3ecafc1b1213
Iamneo <techadmin@iamneo.ai>
2023-12-01 10:47:45 +0000

    Manual-save-commit-12/1/2023--4:17:44-PM

--- springapp/src/main/java/com/example/springapp/Calculator.java
+++ springapp/src/main/java/com/example/springapp/Calculator.java
@@ -6,6 +6,7 @@ public interface Calculator {
 public static void main(String[] args) {
     System.out.println("welcome");
     System.out.println("qwerty");
+    System.out.println("qazxswedc");
 }
 
 }
